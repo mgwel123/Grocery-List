ab96148aeed84eaf8f38e8914354326c96335878
Michael Welker <mikegw224@gmail.com>
2022-04-09 12:04:35 -0700

    finished total cost update function

--- grocerylist.js
+++ grocerylist.js
@@ -4,13 +4,23 @@ document.getElementById('add').addEventListener('click', () => {
     let table = document.getElementById('grocery-list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-grocery-item').value;
-    row.insertCell(1).innerHTML = document.getElementById('cost').value;
-    row.insertCell(2).innerHTML = document.getElementById('quantity').value;
+    row.setAttribute('class', 'list-row');
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.setAttribute('id', 'grocery');
+    cell2.setAttribute('id', 'item-price');
+    cell3.setAttribute('id', 'item-quantity');
+    cell1.innerHTML = document.getElementById('new-grocery-item').value;
+    cell2.innerHTML = document.getElementById('cost').value;
+    cell3.innerHTML = document.getElementById('quantity').value;
+    
     let actions = row.insertCell(3);
     actions.appendChild(createDeleteButton(id++));
     document.getElementById('new-grocery-item').value = '';
     document.getElementById('cost').value = '';
+    document.getElementById('quantity').value = '';
+    updateListTotal();
 });
 
 function createDeleteButton(id) {
@@ -22,24 +32,35 @@ function createDeleteButton(id) {
         console.log(`Removing item-${id} from List`);
         let elementToDelete = document.getElementById(`item-${id}`);
         elementToDelete.parentNode.removeChild(elementToDelete);
+        updateListTotal();
     }
     return btn;
 }
 
+
 let table = document.getElementById('grocery-list');
 let bottomRow = table.insertRow(-1);
 bottomRow.insertCell(0).innerHTML = 'Total Cost:';
-bottomRow.insertCell(1).innerHTML = '$0.00';      //WIP
+let totalCost = bottomRow.insertCell(1);
+totalCost.setAttribute('id', 'total-cost');
+totalCost.innerHTML = '$0.00';      //WIP
 
 function updateListTotal () {
-    let listItemRows = document.getElementById(`item-${id}`);
+    let listItemRows = document.getElementsByClassName(`list-row`);
+    console.log(listItemRows);
+    let sum = 0;
     for (let i = 0; i < listItemRows.length; i++) {
         let listItemRow = listItemRows[i];
-        let itemCost = listItemRow[1].innerText;
-        let itemQuantity = listItemRow[2].innerText;
+        console.log(listItemRow);
+        let costElement = listItemRow.querySelector('#item-price');
+        let quantityElement = listItemRow.querySelector('#item-quantity');
+        console.log(costElement, quantityElement);
+        let itemCost = parseFloat(costElement.innerText);
+        let itemQuantity = parseFloat(quantityElement.innerText);
         console.log(itemCost, itemQuantity);
-
-
+        sum = sum + (itemCost * itemQuantity); 
     }
+    sum = Math.round(sum * 100) / 100;
+    totalCost.innerText = '$' + sum;
 }
 
